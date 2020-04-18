f6a9a6222acaa5f78f9a4f043bcab956e2b4e89d
andrea teruzzi <andre.teru93@gmail.com>
2020-04-18 22:57:55 +0200

    matrici rotanti!

--- Assets/_Ludum Dare 46/Scripts/Enemy/Attack/AttackAction_Bh.cs
+++ Assets/_Ludum Dare 46/Scripts/Enemy/Attack/AttackAction_Bh.cs
@@ -9,11 +9,23 @@
 	public class AttackAction_Bh : AbsAction_Bh
 	{
 		//pattern attacco da effettuare
-		[Title("Parameters"), Range(1,10), SerializeField, HideIf("attackPattern")]
+		[Title("Parameters"), Range(1, 10), SerializeField, HideIf("attackPattern")]
 		private int radius;
 
 		[Title("Parameters"), SerializeField, ShowIf("attackPattern")]
 		private bool[,] attackPattern;
+
+		/// <summary>
+		/// WIP
+		/// </summary>
+		public Vector2Int[] selectedCells {
+			get {
+				List<Vector2Int> tmp = new List<Vector2Int>();
+
+
+				return null;
+			}
+		}
 
 
 		[Title("Next Attack Index"), Range(0, 255), SerializeField, Space]
@@ -22,6 +34,7 @@
 		{
 			((EnemyEntity)Entity).attackActionIndex = nextAttackIndexValue;
 		}
+
 
 		#region EDITOR
 		[Button("Set Attack Pattern", ButtonSizes.Large), HideIf("attackPattern"), GUIColor(0.5f, 1f, 0.5f, 1f)]
@@ -36,6 +49,67 @@
 			attackPattern = null;
 		}
 
+		[Button("Rotate Clockwise!", ButtonSizes.Small), ShowIf("attackPattern"), GUIColor(1f, 1f, 0.9f, 1f)]
+		private void RotateClockwise()
+		{
+			attackPattern.RotateMatrix(radius * 2 + 1);
+		}
+
+		[Button("Rotate Unclockwise!", ButtonSizes.Small), ShowIf("attackPattern"), GUIColor(1f, 1f, 0.9f, 1f)]
+		private void RotateUnclockwise()
+		{
+			attackPattern.RotateMatrix(radius * 2 + 1);
+			attackPattern.RotateMatrix(radius * 2 + 1);
+			attackPattern.RotateMatrix(radius * 2 + 1);
+		}
+
 		#endregion
+
+
 	}
+
+
+	public static class MatrixExtention {
+		// An Inplace function to 
+		// rotate a N x N matrix 
+		// by 90 degrees in anti- 
+		// clockwise direction 
+		public static void RotateMatrix<T>(this T[,] mat, int N)
+		{
+			// Consider all 
+			// squares one by one 
+			for (int x = 0; x < N / 2; x++)
+			{
+				// Consider elements 
+				// in group of 4 in 
+				// current square 
+				for (int y = x; y < N - x - 1; y++)
+				{
+					// store current cell 
+					// in temp variable 
+					T temp = mat[x, y];
+
+					// move values from 
+					// right to top 
+					mat[x, y] = mat[y, N - 1 - x];
+
+					// move values from 
+					// bottom to right 
+					mat[y, N - 1 - x] = mat[N - 1 - x,
+											N - 1 - y];
+
+					// move values from 
+					// left to bottom 
+					mat[N - 1 - x,
+						N - 1 - y]
+						= mat[N - 1 - y, x];
+
+					// assign temp to left 
+					mat[N - 1 - y, x] = temp;
+				}
+			}
+		}
+	}
+
+
 }
