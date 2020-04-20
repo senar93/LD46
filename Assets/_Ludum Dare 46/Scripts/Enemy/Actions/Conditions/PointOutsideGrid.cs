a1bfbc8cce446683be8e930f42f9dcbad0818eaa
andrea teruzzi <andre.teru93@gmail.com>
2020-04-20 15:16:45 +0200

    script per controllare se il nemico punta a una casella fuori dalal griglia

---
+++ Assets/_Ludum Dare 46/Scripts/Enemy/Actions/Conditions/PointOutsideGrid.cs
@@ -0,0 +1,49 @@
+﻿namespace LD46.Conditions
+{
+	using Deirin.EB;
+	using Sirenix.OdinInspector;
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using SenarCustomSystem.Utility;
+	using static SenarCustomSystem.Utility.ComparisonTypeUtility;
+
+	[AddComponentMenu("_Ludum Dare 46/Enemy/Conditions/Point Outside Grid")]
+	public class PointOutsideGrid : AbsCondition
+	{
+		public override bool Check()
+		{
+			EnemyEntity enemyEntity = Entity as EnemyEntity;
+
+			switch (enemyEntity.enemyDirection)
+			{
+				case DirectionEnum.Down:
+					if( enemyEntity.cell.y == 0)
+					{
+						return true;
+					}
+					break;
+				case DirectionEnum.Right:
+					if(enemyEntity.cell.y == enemyEntity.currentGrid.cells.GetLength(0))
+					{
+						return true;
+					}
+					break;
+				case DirectionEnum.Up:
+					if( enemyEntity.cell.y == enemyEntity.currentGrid.cells.GetLength(1))
+					{
+						return true;
+					}
+					break;
+				case DirectionEnum.Left:
+					if( enemyEntity.cell.x == 0)
+					{
+						return true;
+					}
+					break;
+			}
+
+			return false;
+		}
+	}
+}
