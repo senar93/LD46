48f4f754037c2cf224138e75c9db7ff22de44a94
Marco Marchitelli <marchitelli97@libero.it>
2020-04-20 20:54:53 +0200

    Sounds again. Enemy selection DONE.

---
+++ Assets/_Ludum Dare 46/Scripts/EnemyComparer.cs
@@ -0,0 +1,17 @@
+﻿namespace LD46 {
+    using UnityEngine;
+    using UnityEngine.Events;
+
+    public class EnemyComparer : MonoBehaviour {
+        public EnemyEntity thisEnemy;
+        public UnityEvent OnTrue;
+        public UnityEvent OnFalse;
+
+        public void Compare ( EnemyEntity enemyToCompare ) {
+            if ( thisEnemy == enemyToCompare )
+                OnTrue.Invoke();
+            else
+                OnFalse.Invoke();
+        }
+    }
+}
